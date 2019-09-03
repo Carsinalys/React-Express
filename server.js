fb47d3679be0535efa062a80b463fea88e87858a
cardinalys <Cardinalys81@gmail.com>
2019-09-03 19:05:22 +0300

    03.09.2019-work

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 import Express from "express";
 import React from "react";
 import { renderToString } from "react-dom/server";
@@ -8,6 +9,54 @@ import path from "path";
 import Obj from "./server/random_gen";
 const morgan = require("morgan");
 require("dotenv").config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(con => console.log(con.connections))
+  .catch(err => console.log(err));
+
+const ordersShchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Pizza must have a name"],
+    unique: false
+  },
+  cost: {
+    type: Number,
+    default: 9
+  },
+  diameter: Number,
+  weight: Number,
+  ingredients: Object
+});
+
+const Orders = mongoose.model("Orders", ordersShchema);
+
+const testOrder = new Orders({
+  name: "Fucking pizza",
+  cost: 10,
+  diameter: 35,
+  weight: 900,
+  ingredients: {
+    one: 1,
+    two: 2,
+    tree: 3
+  }
+});
+
+testOrder
+  .save()
+  .then(doc => console.log(doc))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 3000;
 
