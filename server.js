dcfd3a10e000f446855339c151b7a7eb634b8121
cardinalys <Cardinalys81@gmail.com>
2019-10-16 18:36:04 +0300

    16.10.2019-work cluster implementing

--- server.js
+++ server.js
@@ -1,65 +1,70 @@
 const mongoose = require("mongoose");
+const cluster = require("cluster");
 
-process.on("uncaughtException", err => {
-  console.log(err);
-  console.log("In uncaught exception...");
-  process.exit(1);
-});
-
-require("dotenv").config({ path: "./config.env" });
-const app = require("./server/index");
-const WebSocket = require("ws").Server;
-const Message = require("./server/models/message");
-const catchAsync = require("./server/utils/catchErrors");
-
-const DB = process.env.DATABASE.replace(
-  "<password>",
-  process.env.DATABASE_PASSWORD
-);
-
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
-  .then(con => {
-    console.log("connected to mongo atlas");
-    //console.log(con.connections)
-  })
-  .catch(err => console.log(err));
-
-const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, () => console.log("lisening on ", PORT));
-
-process.on("unhandledRejection", err => {
-  console.log(err);
-  console.log("In unhandled rejection...");
-  server.close(() => {
+if (cluster.isMaster) {
+  cluster.fork();
+} else {
+  process.on("uncaughtException", err => {
+    console.log(err);
+    console.log("In uncaught exception...");
     process.exit(1);
   });
-});
-// this is heroku event with shoting down server every 24 hours
-process.on("SIGTERM", () => {
-  console.log("SIGTERM recieved, shouting down.");
-  server.close(() => {
-    console.log("process terminated.");
-  });
-});
 
-const socketIo = require("socket.io");
+  require("dotenv").config({ path: "./config.env" });
+  const app = require("./server/index");
+  const WebSocket = require("ws").Server;
+  const Message = require("./server/models/message");
+  const catchAsync = require("./server/utils/catchErrors");
 
-const io = socketIo(server);
+  const DB = process.env.DATABASE.replace(
+    "<password>",
+    process.env.DATABASE_PASSWORD
+  );
 
-io.on("connection", async function(socket) {
-  socket.emit("messageFromExpress", { socket: "connected" });
-  socket.on("messageFromReact", async data => {
-    const newMessage = await Message.create(data);
-    io.emit("messageToState", newMessage);
+  mongoose
+    .connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    })
+    .then(con => {
+      console.log("connected to mongo atlas");
+      //console.log(con.connections)
+    })
+    .catch(err => console.log(err));
+
+  const PORT = process.env.PORT || 3000;
+
+  const server = app.listen(PORT, () => console.log("lisening on ", PORT));
+
+  process.on("unhandledRejection", err => {
+    console.log(err);
+    console.log("In unhandled rejection...");
+    server.close(() => {
+      process.exit(1);
+    });
   });
-  socket.on("disconnect", () => {
-    console.log("disconnected");
+  // this is heroku event with shoting down server every 24 hours
+  process.on("SIGTERM", () => {
+    console.log("SIGTERM recieved, shouting down.");
+    server.close(() => {
+      console.log("process terminated.");
+    });
   });
-});
+
+  const socketIo = require("socket.io");
+
+  const io = socketIo(server);
+
+  io.on("connection", async function(socket) {
+    socket.emit("messageFromExpress", { socket: "connected" });
+    socket.on("messageFromReact", async data => {
+      const newMessage = await Message.create(data);
+      io.emit("messageToState", newMessage);
+    });
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+    });
+  });
+}
